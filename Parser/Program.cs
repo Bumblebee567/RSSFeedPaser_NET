aaf1b91b9190e68c5ad9a91af990510c3e86c4ec
Bumblebee567 <maciekgasiorek9@gmail.com>
2018-03-13 00:37:12 +0100

    Modifications to show working

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -16,14 +16,15 @@ namespace Parser
         {
             Timer refreshTimer = new Timer();
             refreshTimer.Elapsed += RefreshTimer_Elapsed;
-            refreshTimer.Interval = 300000;
+            refreshTimer.Interval = 30000;
             refreshTimer.Start();
-            //Console.ReadKey();
+            Console.ReadKey();
         }
         private static void RefreshTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
             DatabaseHelper.AddChannelsToDatabase(url);
             DatabaseHelper.AddFeedsToDatabase();
+            Console.WriteLine("done");
         }
     }
 }
